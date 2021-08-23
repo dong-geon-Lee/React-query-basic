c7b2af00e5c148571bc08a8bfe63efaad20ef18b
dong-geon-Lee <ch9716@naver.com>
2021-08-23 21:52:05 +0900

    react-query

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import { useMutation, useQuery } from "react-query";
+import "./App.css";
 
 function App() {
+  const { isLoading, isError, data } = useQuery("dogs", () =>
+    axios("https://random.dog/woof.json")
+  );
+
+  if (isLoading) return <h1>Loading...</h1>;
+  if (isError) return <h1>Error: {isError}, try again!</h1>;
+
+  const { url } = data.data;
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <img src={url} style={{ width: "250px" }} alt="" />
     </div>
   );
 }
